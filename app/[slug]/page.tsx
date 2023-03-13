44efd71e77248f214366d554e87f96bbc89ee62f
Danny Little <dannyl9979@gmail.com>
2023-03-13 14:46:53 -0400

    simplify code

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,21 +1,13 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
-
-function page() {
-  return <div>page</div>;
+export default function Page() {
+  return;
 }
-
-export default page;
 
 export async function generateStaticParams() {
-  await generateRssFeed();
-
-  return [{ slug: "route" }];
-}
-
-async function generateRssFeed() {
   const ReactDomServer = (await import("react-dom/server")).default;
   const element = await (<MDXRemote source={`#hi`} />);
-  //   const element = <div>hi</div>;
   const staticMarkup = ReactDomServer.renderToStaticMarkup(element);
   console.log(staticMarkup);
+
+  return [{ slug: "route" }];
 }
